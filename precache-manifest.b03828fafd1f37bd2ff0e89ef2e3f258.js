105b0f2b71d0f65623b8475daa346b33e0b4130e
Nguyễn Thanh Liêm <liemnt.uit@gmail.com>
2019-12-10 20:03:32 +0700

    Updates

---
+++ precache-manifest.b03828fafd1f37bd2ff0e89ef2e3f258.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a319aef6e53e4e8c7c86acb5cb7e2158",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "63691d54be69e53a4545",
+    "url": "/videos/static/css/2.568a7919.chunk.css"
+  },
+  {
+    "revision": "a6b49fd5928d95612419",
+    "url": "/videos/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "63691d54be69e53a4545",
+    "url": "/videos/static/js/2.1af6faf1.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/videos/static/js/2.1af6faf1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6b49fd5928d95612419",
+    "url": "/videos/static/js/main.137919c9.chunk.js"
+  },
+  {
+    "revision": "de190ba1c8ceaefb91a8",
+    "url": "/videos/static/js/runtime-main.22ee7d76.js"
+  }
+]);
